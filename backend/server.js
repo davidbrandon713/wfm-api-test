11d15d1a126017fcf2a0de6a0ef8c13bc31ace37
David <davidbrandon713@gmail.com>
2023-03-26 01:48:21 -0500

    removing unused endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,66 +6,49 @@ const port = 3002
 app.use(express.json())
 app.use(cors())
 
-app.get('/all', async (req, res) => {
-	try {
-		await fetch(`https://api.warframe.market/v1/items/ash_prime_set/orders`, {
-			method: 'GET',
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => res.status(200).json(data.payload.orders))
-			.then(console.log(`GET nekros orders`))
-	} catch (err) {
-		res.status(500).json({ message: err.message })
-	}
-})
-
 app.get('/:item', async (req, res) => {
-	let itemDat
-	try {
-		await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
-			method: 'GET',
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		})
-			.then((res) => res.json())
-			.then((data) =>
-				data.payload.item.items_in_set.filter((item) => {
-					return item.url_name === req.params.item
-				})
-			)
-			.then((data) => res.status(200).json(data[0].en.item_name))
-	} catch (err) {
-		res.status(500).json({ message: err.message })
-	}
+  try {
+    await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        data.payload.item.items_in_set.filter((item) => {
+          return item.url_name === req.params.item
+        })
+      )
+      .then((data) => res.status(200).json(data[0].en.item_name))
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
-app.get('/:item/orders/sell', async (req, res) => {
-	try {
-		await fetch(
-			`https://api.warframe.market/v1/items/${req.params.item}/orders`,
-			{
-				method: 'GET',
-				headers: {
-					'Access-Control-Allow-Origin': '*',
-				},
-			}
-		)
-			.then((res) => res.json())
-			.then((data) =>
-				data.payload.orders.filter((order) => {
-					return order.user.status === 'ingame'
-				})
-			)
+app.get('/:item/orders', async (req, res) => {
+  try {
+    await fetch(
+      `https://api.warframe.market/v1/items/${req.params.item}/orders`,
+      {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        data.payload.orders.filter((order) => {
+          return order.user.status === 'ingame'
+        })
+      )
       .then((data) => res.status(200).json(data))
-	} catch (err) {
-		res.status(500).json({ message: err.message })
-	}
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 app.listen(port, () => {
-	console.log(`\x1b[33mServer running on port ${port}.\x1b[39m`)
+  console.log(`\x1b[33mServer running on port ${port}.\x1b[39m`)
 })
--- frontend/src/components/scroll.js
+++ frontend/src/components/scroll.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react'
 
 const Scroll = ({ children, dark }) => {
-	const [windowSize, setWindowSize] = useState({
-		height: window.innerHeight,
-	})
+  const [windowSize, setWindowSize] = useState({
+    height: window.innerHeight,
+  })
 
-	useEffect(() => {
-		window.onresize = () => {
-			setWindowSize({
-				height: window.innerHeight,
-			})
-		}
-	}, [])
+  useEffect(() => {
+    window.onresize = () => {
+      setWindowSize({
+        height: window.innerHeight,
+      })
+    }
+  }, [])
 
-	return (
-		<div
-			className="scroll-container"
-			style={{
-				height: windowSize.height - 60,
-        scrollbarColor: dark ? '#fffafa #333' : 'rgb(48, 43, 49) #fffafa'
-			}}
-		>
-			{children}
-		</div>
-	)
+  return (
+    <div
+      className="scroll-container"
+      style={{
+        height: windowSize.height - 60,
+        scrollbarColor: dark ? '#fffafa #333' : 'rgb(48, 43, 49) #fffafa',
+      }}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default Scroll
